34171b9077e5f0b8ed52c14850237df0b5c04201
Sarah GLINER <sarah.gliner@ledger.fr>
2022-12-26 10:40:51 +0100

    ui: refactore to have bagl related code in dedicated file

--- src/ui_common.c
+++ src/ui_common.c
@@ -10,31 +10,64 @@ void io_seproxyhal_display(const bagl_element_t *element) {
     return io_seproxyhal_display_default((bagl_element_t *) element);
 }
 
-void ui_init(void) {
-    UX_INIT();
+void ui_refresh(void) {
+    ux_stack_display(0);
 }
 
-// User MUST call `init_screen_stack()` before the first call to this function.
-void push_ui_callback(char *title, string_generation_callback cb, void *data) {
-    if (global.dynamic_display.formatter_index + 1 >= MAX_SCREEN_STACK_SIZE) {
-        THROW(0x6124);
+// CALLED BY THE SDK
+unsigned char io_event(unsigned char channel);
+
+unsigned char io_event(__attribute__((unused)) unsigned char channel) {
+    // nothing done with the event, throw an error on the transport layer if
+    // needed
+
+    // can't have more than one tag in the reply, not supported yet.
+    switch (G_io_seproxyhal_spi_buffer[0]) {
+        case SEPROXYHAL_TAG_FINGER_EVENT:
+            UX_FINGER_EVENT(G_io_seproxyhal_spi_buffer);
+            break;
+
+        case SEPROXYHAL_TAG_BUTTON_PUSH_EVENT:
+            UX_BUTTON_PUSH_EVENT(G_io_seproxyhal_spi_buffer);
+            break;
+
+        case SEPROXYHAL_TAG_STATUS_EVENT:
+            if (G_io_apdu_media == IO_APDU_MEDIA_USB_HID &&
+                !(U4BE(G_io_seproxyhal_spi_buffer, 3) &
+                  SEPROXYHAL_TAG_STATUS_EVENT_FLAG_USB_POWERED)) {
+                THROW(EXCEPTION_IO_RESET);
+            }
+            // no break is intentional
+        default:
+            UX_DEFAULT_EVENT();
+            break;
+
+        case SEPROXYHAL_TAG_DISPLAY_PROCESSED_EVENT:
+            UX_DISPLAYED_EVENT({});
+            break;
+
+        case SEPROXYHAL_TAG_TICKER_EVENT:
+#ifdef BAKING_APP
+            // Disable ticker event handling to prevent screen saver from starting.
+#else
+            UX_TICKER_EVENT(G_io_seproxyhal_spi_buffer, {});
+#endif
+
+            break;
     }
-    struct screen_data *fmt =
-        &global.dynamic_display.screen_stack[global.dynamic_display.formatter_index];
 
-    fmt->title = title;
-    fmt->callback_fn = cb;
-    fmt->data = data;
-    global.dynamic_display.formatter_index++;
-    global.dynamic_display.screen_stack_size++;
+    // close the event if not done previously (by a display or whatever)
+    if (!io_seproxyhal_spi_is_status_sent()) {
+        io_seproxyhal_general_status();
+    }
+
+    // command has been processed, DO NOT reset the current APDU transport
+    return 1;
 }
-
-void init_screen_stack() {
-    explicit_bzero(&global.dynamic_display.screen_stack,
-                   sizeof(global.dynamic_display.screen_stack));
-    global.dynamic_display.formatter_index = 0;
-    global.dynamic_display.screen_stack_size = 0;
-    global.dynamic_display.current_state = STATIC_SCREEN;
+void ui_init(void) {
+#ifdef HAVE_BAGL
+        UX_INIT();
+#endif  // HAVE_BAGL
 }
 
 void require_pin(void) {
