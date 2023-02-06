97220b350ebdf0aef27f3c8c502f0f0a64fe42cf
Sarah GLINER <sarah.gliner@ledger.fr>
2023-02-06 13:56:34 +0100

    bluetooth: fix support for Stax

--- src/swap/handle_swap_sign_transaction.c
+++ src/swap/handle_swap_sign_transaction.c
@@ -33,13 +33,11 @@ void handle_swap_sign_transaction(void) {
     USB_power(0);
     USB_power(1);
     PRINTF("USB power ON/OFF\n");
-#ifdef TARGET_NANOX
+#ifdef HAVE_BLE
     // grab the current plane mode setting
     G_io_app.plane_mode = os_setting_get(OS_SETTING_PLANEMODE, NULL, 0);
-#endif  // TARGET_NANOX
-#ifdef HAVE_BLE
     BLE_power(0, NULL);
-    BLE_power(1, "Nano X");
+    BLE_power(1, NULL);
 #endif  // HAVE_BLE
     app_main();
 }
