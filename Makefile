nndiaye-ledger/app-tezos

Stax: add support for NBGL

Makefile
@@ -46,6 +46,8 @@ endif
 
 ifeq ($(TARGET_NAME),TARGET_NANOS)
 ICONNAME=icons/nano-s-tezos.gif
+else ifeq ($(TARGET_NAME),TARGET_FATSTACKS)
+ICONNAME=icons/fat_tezos.gif
 else
 ICONNAME=icons/nano-x-tezos.gif
 endif
@@ -66,7 +68,12 @@ show-app:
 ############
 
 DEFINES   += OS_IO_SEPROXYHAL
-DEFINES   += HAVE_BAGL HAVE_SPRINTF HAVE_UX_FLOW
+ifneq ($(TARGET_NAME),TARGET_FATSTACKS)
+DEFINES += HAVE_BAGL HAVE_UX_FLOW
+else
+DEFINES += NBGL_QRCODE
+endif
+DEFINES   += HAVE_SPRINTF
 DEFINES   += HAVE_IO_USB HAVE_L4_USBLIB IO_USB_MAX_ENDPOINTS=6 IO_HID_EP_LENGTH=64 HAVE_USB_APDU
 DEFINES   += HAVE_LEGACY_PID
 DEFINES   += VERSION=\"$(APPVERSION)\" APPVERSION_M=$(APPVERSION_M)
@@ -85,12 +92,15 @@ DEFINES   += IO_SEPROXYHAL_BUFFER_SIZE_B=128
 else
 DEFINES   += IO_SEPROXYHAL_BUFFER_SIZE_B=300
 DEFINES   += HAVE_GLO096
-DEFINES   += HAVE_BAGL BAGL_WIDTH=128 BAGL_HEIGHT=64
+ifneq ($(TARGET_NAME),TARGET_FATSTACKS)
+DEFINES       += HAVE_BAGL BAGL_WIDTH=128 BAGL_HEIGHT=64
+endif
 DEFINES   += HAVE_BAGL_ELLIPSIS # long label truncation feature
 DEFINES   += HAVE_BAGL_FONT_OPEN_SANS_REGULAR_11PX
 DEFINES   += HAVE_BAGL_FONT_OPEN_SANS_EXTRABOLD_11PX
 DEFINES   += HAVE_BAGL_FONT_OPEN_SANS_LIGHT_16PX
 endif
+DEFINES   += UNUSED\(x\)=\(void\)x
 
 # Enabling debug PRINTF
 DEBUG ?= 0
@@ -150,7 +160,13 @@ include $(BOLOS_SDK)/Makefile.glyphs
 
 ### computed variables
 APP_SOURCE_PATH  += src
-SDK_SOURCE_PATH  += lib_stusb lib_stusb_impl lib_ux
+SDK_SOURCE_PATH  += lib_stusb lib_stusb_impl
+
+ifeq ($(TARGET_NAME),TARGET_FATSTACKS)
+SDK_SOURCE_PATH += lib_nbgl/src
+else
+SDK_SOURCE_PATH += lib_ux
+endif
 
 ### U2F support (wallet app only)
 ifeq ($(APP), tezos_wallet)

trilitech/ledger-app-tezos-baking

Makefile
@@ -46,6 +46,8 @@ endif
 
 ifeq ($(TARGET_NAME),TARGET_NANOS)
 ICONNAME=icons/nano-s-tezos.gif
+else ifeq ($(TARGET_NAME),TARGET_STAX)
+ICONNAME=icons/stax_tezos.gif
 else
 ICONNAME=icons/nano-x-tezos.gif
 endif
@@ -66,7 +68,7 @@ show-app:
 ############
 
 DEFINES   += OS_IO_SEPROXYHAL
-DEFINES   += HAVE_BAGL HAVE_SPRINTF HAVE_UX_FLOW
+DEFINES   += HAVE_SPRINTF
 DEFINES   += HAVE_IO_USB HAVE_L4_USBLIB IO_USB_MAX_ENDPOINTS=6 IO_HID_EP_LENGTH=64 HAVE_USB_APDU
 DEFINES   += HAVE_LEGACY_PID
 DEFINES   += VERSION=\"$(APPVERSION)\" APPVERSION_M=$(APPVERSION_M)
@@ -84,13 +86,23 @@ ifeq ($(TARGET_NAME),TARGET_NANOS)
 DEFINES   += IO_SEPROXYHAL_BUFFER_SIZE_B=128
 else
 DEFINES   += IO_SEPROXYHAL_BUFFER_SIZE_B=300
-DEFINES   += HAVE_GLO096
-DEFINES   += HAVE_BAGL BAGL_WIDTH=128 BAGL_HEIGHT=64
-DEFINES   += HAVE_BAGL_ELLIPSIS # long label truncation feature
-DEFINES   += HAVE_BAGL_FONT_OPEN_SANS_REGULAR_11PX
-DEFINES   += HAVE_BAGL_FONT_OPEN_SANS_EXTRABOLD_11PX
-DEFINES   += HAVE_BAGL_FONT_OPEN_SANS_LIGHT_16PX
 endif
+
+ifeq ($(TARGET_NAME),TARGET_STAX)
+    DEFINES += NBGL_QRCODE
+else
+    DEFINES += HAVE_BAGL HAVE_UX_FLOW
+    ifneq ($(TARGET_NAME),TARGET_NANOS)
+        DEFINES   += HAVE_GLO096
+        DEFINES   += BAGL_WIDTH=128 BAGL_HEIGHT=64
+        DEFINES   += HAVE_BAGL_ELLIPSIS # long label truncation feature
+        DEFINES   += HAVE_BAGL_FONT_OPEN_SANS_REGULAR_11PX
+        DEFINES   += HAVE_BAGL_FONT_OPEN_SANS_EXTRABOLD_11PX
+        DEFINES   += HAVE_BAGL_FONT_OPEN_SANS_LIGHT_16PX
+    endif
+endif
+
+DEFINES   += UNUSED\(x\)=\(void\)x
 
 # Enabling debug PRINTF
 DEBUG ?= 0
@@ -150,7 +162,11 @@ include $(BOLOS_SDK)/Makefile.glyphs
 
 ### computed variables
 APP_SOURCE_PATH  += src
-SDK_SOURCE_PATH  += lib_stusb lib_stusb_impl lib_ux
+SDK_SOURCE_PATH  += lib_stusb lib_stusb_impl
+
+ifneq ($(TARGET_NAME),TARGET_STAX)
+SDK_SOURCE_PATH += lib_ux
+endif
 
 ### U2F support (wallet app only)
 ifeq ($(APP), tezos_wallet)